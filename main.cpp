f45b081b64dbb65b92e3cdce0e7c834a55b495e6
kidlatmc29 <kidlatmc@gmail.com>
2022-11-30 16:17:14 -0800

    setting up project with fxns and make file

---
+++ main.cpp
@@ -0,0 +1,42 @@
+// Isabel Ovalles
+// CPSC 4100 - Final Project 
+
+// References: 
+// 
+
+#include <iostream> 
+#include <iomanip>
+#include <fstream>
+
+using namespace std; 
+
+struct Stats
+{
+    int numOfBins; 
+    double elapsedTime; 
+};
+
+int main()
+{
+    cout << "CPSC 4100 - Final Project: Comparing Bin-Packing " << endl;
+
+    return 0;
+}
+
+// reading in list of data
+void readData()
+{
+    
+}
+
+// 1.) intractable but optimal solution 
+Stats findMinNumBins()
+{
+
+}
+
+// 2.) use heuristic from resourse 
+Stats findFirstFit()
+{
+
+}
